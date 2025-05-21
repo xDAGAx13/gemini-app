ac0a5f0129c89b6bbe89c09e658596c7daf32b76
xDAGAx13 <157238871+xDAGAx13@users.noreply.github.com>
2025-05-21 18:34:56 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,5 @@
 'use client'
-import { useRouter } from 'next/navigation'
-import { useEffect } from 'react';
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(()=>{
-    router.push('/login'); 
-  },[router])
-
-  return null;
+  redirect('/login');
 }
